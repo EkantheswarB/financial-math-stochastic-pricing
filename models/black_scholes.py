f0302e65f3825f7de7b9733484777e98723bdbb8
EkantheswarB <143138659+EkantheswarB@users.noreply.github.com>
2025-10-18 15:49:46 -0400

    Update black_scholes.py

--- models/black_scholes.py
+++ models/black_scholes.py
@@ -1,8 +1,13 @@
 import numpy as np
 from scipy.stats import norm
 
+def _d1_d2(S, K, T, r, sigma):
+    d1 = (np.log(S/K) + (r + 0.5 * sigma**2)*T) / (sigma*np.sqrt(T))
+    d2 = d1 - sigma*np.sqrt(T)
+    return d1, d2
+
 def black_scholes_price(S, K, T, r, sigma, option_type='call'):
-    """
+     """
     S: Spot Price
     K: Strike Price
     T: Time to Maturity (in years)
@@ -10,11 +15,10 @@ def black_scholes_price(S, K, T, r, sigma, option_type='call'):
     sigma: Volatility
     option_type: 'call' or 'put'
     """
-    d1 = (np.log(S/K) + (r + 0.5 * sigma**2) * T) / (sigma * np.sqrt(T))
-    d2 = d1 - sigma * np.sqrt(T)
-    
+    d1, d2 = _d1_d2(S, K, T, r, sigma)
     if option_type == 'call':
-        return S * norm.cdf(d1) - K * np.exp(-r*T) * norm.cdf(d2)
-    elif option_type == 'put':
-        return K * np.exp(-r*T) * norm.cdf(-d2) - S * norm.cdf(-d1)
+        return S*norm.cdf(d1) - K*np.exp(-r*T)*norm.cdf(d2)
+    else:
+        return K*np.exp(-r*T)*norm.cdf(-d2) - S*norm.cdf(-d1)
+
 
